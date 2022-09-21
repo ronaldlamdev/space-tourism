427245641f2d44dc2aa07e546b3cf12d5e906941
milessteamaccount <ronaldlamdev@gmail.com>
2022-09-20 21:21:56 -0500

    added background for different screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './routes/Home'
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
