0a442cc0ae4aa942d9ab05b4d99ecc717e737a66
milessteamaccount <ronaldlamdev@gmail.com>
2022-09-22 19:20:55 -0500

    added close open function using usestate for mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,12 @@ import {FaBars} from 'react-icons/fa'
 import {AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  }
+
   return (
     <nav className='flex justify-between items-center p-4'>
       <Link to="/">
@@ -26,27 +32,31 @@ const Navbar = () => {
         </li>
       </ul>
 
-      <FaBars size={25} className="text-white" />
+      <FaBars onClick={handleNav} size={25} className={!nav ? 'block text-white' : 'hidden'} />
 
       {/* Mobile Menu */}
 
       {/* Overlay */}
-      <div className='absolute w-full h-screen top-0 left-0 bg-black/60 hidden'></div>
+      <div className={nav ? 'absolute w-full h-screen top-0 left-0 bg-black/60 block' : 'absolute w-full h-screen top-0 left-0 bg-black/60 hidden'}></div>
 
-      <div className='absolute top-0 left-0 right-0 text-white w-full h-screen font-["Barlow_Condensed"] hidden'>
-        <AiOutlineClose className='absolute top-[1.65rem] right-[.85rem]' size={30}/>
+      <div className={
+            nav ? 'absolute top-0 left-0 right-0 text-white w-full h-screen font-["Barlow_Condensed"] duration-300' 
+            : 
+            'absolute top-[-100%] left-0 right-0 text-white w-full h-screen font-["Barlow_Condensed"] duration-300'
+          }>
+        <AiOutlineClose onClick={handleNav} className='absolute top-[1.65rem] right-[.85rem]' size={30}/>
         <ul className='flex flex-col justify-center items-center w-full h-screen'>
           <li className='py-4 text-2xl'>
-            <Link to="/"><span>00</span> HOME</Link>
+            <Link onClick={handleNav} to="/"><span>00</span> HOME</Link>
           </li>
           <li className='py-4 text-2xl'>
-            <Link to="destination"><span>01</span> DESTINATION</Link>
+            <Link onClick={handleNav} to="destination"><span>01</span> DESTINATION</Link>
           </li>
           <li className='py-4 text-2xl'>
-            <Link to="/crew"><span>02</span> CREW</Link>
+            <Link onClick={handleNav} to="/crew"><span>02</span> CREW</Link>
           </li>
           <li className='py-4 text-2xl'>
-            <Link to="/technology"><span>03</span> TECHNOLOGY</Link>
+            <Link onClick={handleNav} to="/technology"><span>03</span> TECHNOLOGY</Link>
           </li>
         </ul>
       </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,15 +5,17 @@ const Home = () => {
   return (
     <div className='home-bg w-full h-screen'>
       <Navbar />
-      <div className='text-gray-300 font-["Bellefair"]'>
-        <h4>SO, YOU WANT TO TRAVEL TO</h4>
-        <h1>SPACE</h1>
-        <p>
-          Let's face it; if you want to go to space, you might
-          as well genuinely go to outer space and not hover kind of on the
-          edge of it. Well sit back, and relax because we'll give you 
-          a truly out of this world experience!
-        </p>
+      <div className=''>
+        <div className=''>
+          <h4 className='font-["Barlow"] text-gray-300'>SO, YOU WANT TO TRAVEL TO</h4>
+          <h1 className='font-["Bellefair"] text-white'>SPACE</h1>
+          <p className='font-["Barlow_Condensed"] text-gray-300'>
+            Let's face it; if you want to go to space, you might
+            as well genuinely go to outer space and not hover kind of on the
+            edge of it. Well sit back, and relax because we'll give you 
+            a truly out of this world experience!
+          </p>
+        </div>
       </div>
     </div>
   )
