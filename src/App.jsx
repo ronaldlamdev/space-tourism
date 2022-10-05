5944a94c75fe40ab8545f9344d58911303269370
milessteamaccount <ronaldlamdev@gmail.com>
2022-10-04 20:35:12 -0500

    navlink with active class

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/destination" element={<Destination />} /> 
         <Route path="/crew" element={<Crew />} />
         <Route path="/technology" element={<Technology />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import Logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import {FaBars} from 'react-icons/fa'
 import {AiOutlineClose} from 'react-icons/ai'
 
 const Navbar = () => {
-  const [link, setLink] = useState(0);
+
+  // styling for NavLinks
+  const activeLink = "mr-8 relative flex items-center h-full mobile-link-active";
+  const normalLink = "mr-8 relative flex items-center h-full mobile-link-hover duration-300";
 
   const [nav, setNav] = useState(false);
   const handleNav = () => {
@@ -14,15 +16,15 @@ const Navbar = () => {
   }
   return (
     <nav className='flex justify-between items-center p-4 md:p-0'>
-      <Link to="/">
+      <Link to="/home">
         <img className='cursor-pointer md:p-4' src={Logo} alt="logo"/>
       </Link>
       <div className=''></div>
       <ul className='hidden md:flex px-12 justify-evenly text-xl items-center font-["Barlow_Condensed"] text-white bg-[#ffffff0a]/5 backdrop-blur-3xl h-[80px]'>
-          <NavLink className='mr-8 flex items-center h-full hover:border-b-2 duration-300' to="/"><span className='hidden'>00</span> HOME</NavLink>
-          <NavLink className='mr-8 flex items-center h-full hover:border-b-2 duration-300' to="/destination"><span className='hidden'>01</span> DESTINATION</NavLink>
-          <NavLink className='mr-8 flex items-center h-full hover:border-b-2 duration-300' to="/crew"><span className='hidden'>02</span> CREW</NavLink>
-          <NavLink className='mr-8 flex items-center h-full hover:border-b-2 duration-300' to="/technology"><span className='hidden'>03</span> TECHNOLOGY</NavLink>
+        <NavLink className={({isActive}) => (isActive ? activeLink : normalLink)} to="/home"><span className='hidden'>00</span> HOME</NavLink>
+        <NavLink className={({isActive}) => (isActive ? activeLink : normalLink)} to="/destination"><span className='hidden'>01</span> DESTINATION</NavLink>
+        <NavLink className={({isActive}) => (isActive ? activeLink : normalLink)} to="/crew"><span className='hidden'>02</span> CREW</NavLink>
+        <NavLink className={({isActive}) => (isActive ? activeLink : normalLink)} to="/technology"><span className='hidden'>03</span> TECHNOLOGY</NavLink>
       </ul>
 
       <FaBars onClick={handleNav} size={25} className={!nav ? 'block text-white cursor-pointer hover:text-[#a9b5f7] duration-300 md:hidden' : 'hidden'} />
