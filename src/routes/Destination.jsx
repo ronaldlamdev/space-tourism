355447843689584ceda42bc0b62b351bda1803b9
milessteamaccount <ronaldlamdev@gmail.com>
2022-10-05 22:36:48 -0500

    added box shadow hover effect for circle in home page

--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -22,33 +22,33 @@ const Destination = () => {
 
     <div className='destination-bg w-full h-screen'>
       <Navbar />
-      <div className='grid grid-cols-1 px-4'>
+      <div className='grid grid-cols-1 px-4 w-full'>
 
         {/* First grid column */}
-        <div className='text-white flex flex-col items-center md:text-left pt-4'>
-          <h1 className='text-2xl font-["Barlow"]'><span className='text-white/50 mr-2'>01</span>PICK YOUR DESTINATION</h1>
-          <img className='w-[200px] h-auto py-4' src={image} alt={Data.destinations[destination].name} />
+        <div className='text-white flex flex-col items-center w-full pt-4'>
+          <h1 className='text-2xl font-["Barlow"] md:self-start'><span className='text-white/50 mr-2'>01</span>PICK YOUR DESTINATION</h1>
+          <img className='w-[200px] md:w-[300px] h-auto py-4 md:py-12' src={image} alt={Data.destinations[destination].name} />
         </div>
 
         {/* Second grid column */}
         <div className='flex flex-col items-center justify-evenly text-center'>
 
           {/* Destination options */}
-          <ul className='flex text-xl justify-evenly text-white w-full font-["Barlow_Condensed"] py-3 md:justify-center md:gap-20'>
+          <ul className='flex text-xl md:text-2xl justify-evenly text-white w-full font-["Barlow_Condensed"] py-3 md:justify-center md:gap-20'>
             <li onClick={() => {setDestination(0), imageHandler()}} className={'dest-option' + (destination === 0 ? ' dest-active' : "")}>Moon</li>
             <li onClick={() => {setDestination(1), imageHandler(1)}} className={'dest-option' + (destination === 1 ? ' dest-active' : "")}>Mars</li>
             <li onClick={() => {setDestination(2), imageHandler(2)}} className={'dest-option' + (destination === 2 ? ' dest-active' : "")}>Europa</li>
             <li onClick={() => {setDestination(3), imageHandler(3)}} className={'dest-option' + (destination === 3 ? ' dest-active' : "")}>Titan</li>
           </ul>
 
-          <h1 className='text-white font-["Bellefair"] text-[2.3rem]'>{Data.destinations[destination].name}</h1>
-          <p className='text-[#d2d8f9] font-["Barlow"] pb-6 md:px-28'>{Data.destinations[destination].description}</p>
+          <h1 className='text-white font-["Bellefair"] text-[2.3rem] text-6xl md:py-8'>{Data.destinations[destination].name}</h1>
+          <p className='text-[#d2d8f9] font-["Barlow"] pb-6 md:px-28 md:text-xl'>{Data.destinations[destination].description}</p>
 
           {/* Border */}
           <div className='w-full h-[2px] bg-gray-500'></div>
 
           <div className='text-white flex flex-col justify-evenly gap-4 py-4'>
-            <div className=''>
+            <div>
               <h3 className='text-[#d2d8f9] font-["Barlow"]'>AVG DISTANCE</h3>
               <h2 className='font-["Bellefair"] text-[1.75rem]'>{Data.destinations[destination].distance}</h2>
             </div>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
 
         {/* 1st Grid Column */}
         <div className='p-8'>
-          <h4 className='font-["Barlow"] text-[#d2d8f9] mb-6 tracking-widest'>SO, YOU WANT TO TRAVEL TO</h4>
-          <h1 className='font-["Bellefair"] text-white text-8xl mb-6'>SPACE</h1>
+          <h4 className='font-["Barlow"] text-[#d2d8f9] pb-6 tracking-widest'>SO, YOU WANT TO TRAVEL TO</h4>
+          <h1 className='font-["Bellefair"] text-white text-8xl pb-6'>SPACE</h1>
           <p className='font-["Barlow_Condensed"] text-[#d2d8f9] text-xl px-16'>
             Let's face it; if you want to go to space, you might
             as well genuinely go to outer space and not hover kind of on the
@@ -22,7 +22,7 @@ const Home = () => {
 
         {/* 2nd Grid Column */}
         <div className='flex justify-center'>
-          <div className='bg-[#ffffff] rounded-[50%] w-[250px] h-[250px] flex justify-center items-center'>
+          <div className='bg-[#ffffff] rounded-[50%] w-[250px] h-[250px] flex justify-center items-center hover:shadow-[0px_0px_0px_1.5rem_hsla(0,0%,100%,0.1)] hover:ease-out duration-300'>
             <Link to="/destination">
               <h2 className='font-["Bellefair"] text-3xl'>EXPLORE</h2>
             </Link>
