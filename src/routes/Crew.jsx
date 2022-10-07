00905df541924c4d817e59df943e940aa9c24f53
milessteamaccount <ronaldlamdev@gmail.com>
2022-10-07 18:25:11 -0500

    redo mobile stylng of crew component

--- src/routes/Crew.jsx
+++ src/routes/Crew.jsx
@@ -23,13 +23,18 @@ const Crew = () => {
     <div className='crew-bg w-full h-screen'>
       <Navbar />
       <h1 className='text-2xl font-["Barlow"] pt-4 text-white text-center'><span className='text-white/50 mr-2'>02</span>MEET YOUR CREW</h1>
-      <div className='flex flex-col-reverse justify-center items-center p-4'>
+      <div className='flex flex-col justify-center items-center p-4'>
+
+        {/* Image container */}
+        <div className='border-b-2 border-gray-700 w-full mt-6'>
+          <img className='w-[280px] h-[330px] mx-auto' src={image} alt={Data.crew[member].name}/>
+        </div>
 
         {/* Text container */}
         <div className='flex flex-col text-center p-4'>
 
           {/* Options */}
-          <div className='flex justify-evenly mx-auto gap-4'>
+          <div className='flex justify-evenly mx-auto py-8 gap-4'>
             <div onClick={() => {setMember(0), imageHandler()}} className={'crew-option' + (member === 0 ? ' crew-active' : '')}></div>
             <div onClick={() => {setMember(1), imageHandler(1)}} className={'crew-option' + (member === 1 ? ' crew-active' : '')}></div>
             <div onClick={() => {setMember(2), imageHandler(2)}} className={'crew-option' + (member === 2 ? ' crew-active' : '')}></div>
@@ -37,15 +42,10 @@ const Crew = () => {
           </div>
 
           <div className='text-white mt-4'>
-            <h3 className='text-white/50 font-["Bellefair"] text-2xl'>{Data.crew[member].role}</h3>
-            <h2 className='font-["Bellefair"] text-3xl'>{Data.crew[member].name}</h2>
-            <p className='text-[#d2d8f9] mt-4 font-["Barlow_Condensed"] text-xl'>{Data.crew[member].bio}</p>
+            <h3 className='text-white/50 font-["Bellefair"] text-4xl'>{Data.crew[member].role}</h3>
+            <h2 className='font-["Bellefair"] text-[2.5rem]'>{Data.crew[member].name}</h2>
+            <p className='text-[#d2d8f9] mt-4 px-8 font-["Barlow_Condensed"] text-[1.5rem]'>{Data.crew[member].bio}</p>
           </div>
-        </div>
-
-        {/* Image container */}
-        <div className='border-b-2 border-gray-700 w-full'>
-          <img className='w-[280px] h-[330px] mx-auto' src={image} alt={Data.crew[member].name}/>
         </div>
 
       </div>
